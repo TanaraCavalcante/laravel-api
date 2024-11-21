5cb9b3a756187d0b5e8cd4c13aa347f65eaedaf9
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-21 14:52:25 +0100

    Api Show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,7 +17,7 @@ class ProjectController extends Controller
         ]);
     }
     public function show(Project $project){
-        $project = new Project();
+        $project = Project::with("type","technologies")->findOrFail($project->id);
         return response()->json([
             'success'=> true,
             'results'=> $project
